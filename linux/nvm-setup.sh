fe42b8e88da00caf3d43cdfe48f76753763d3e5e
LG <imcoderlg@protonmail.com>
2022-02-19 16:46:14 +0000

    some progress

--- linux/nvm-setup.sh
+++ linux/nvm-setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 # variables
-export node_ver=lts
+export node_ver="lts"
 # Install node14-lts with `nvm`
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 nvm install $node_ver
 # Install `yarn` with `npm`
 npm i -g yarn
--- linux/setup-system.sh
+++ linux/setup-system.sh
@@ -1,13 +1,17 @@
+#!/bin/bash
+# Variables
+
+# Start!
 echo "Setting system up with im-coder-lg's favourite tools..."
 # Add NVM
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")" [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+echo -n "do you want to set up nvm? (1 for yes/2 for no) "
+read nvmyn
+if [$nvmyn==1]
+then
+    sh ./nvm-setup.sh
+fi
 # Add Pyenv
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
-echo "$SHELL"
+
 # Get pre-commit up and running!
 pip3 install pre-commit
 pre-commit --install
